6969b586d10eca4362c7a62a02f5a198d0d6270e
Laura Beatris <laurabeatriserafim@gmail.com>
2019-10-18 23:00:15 -0300

    prettier w/eslint integration and added .editorconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,23 @@
 module.exports = {
   env: {
     es6: true,
-    node: true,
+    node: true
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ["airbnb-base", "prettier"],
+  plugins: ["prettier"],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module"
   },
   rules: {
-  },
+    "prettier/prettier": "error",
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    camelcase: "off",
+    "no-unused-vars": ["error", { argsIgnorePattern: "next" }]
+  }
 };
