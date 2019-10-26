031f438ff3db85b452fdf272d45c3d926d182a51
Laura Beatris <laurabeatriserafim@gmail.com>
2019-10-26 13:40:51 -0300

    sequelize configurations and first migration - creating user table

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'localhost',
+  database: 'goeat',
+  username: 'laura',
+  password: '123',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
