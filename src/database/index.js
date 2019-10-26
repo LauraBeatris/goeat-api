3f8dcdf6c7f81cf1fa37c9a54307bf001870bba4
Laura Beatris <laurabeatriserafim@gmail.com>
2019-10-26 14:17:10 -0300

    created models loader

---
+++ src/database/index.js
@@ -0,0 +1,20 @@
+import Sequelize from 'sequelize';
+
+import databaseConfig from '../config/database';
+import User from '../app/models/User';
+
+const models = [User];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+
+    models.map(model => model.init(databaseConfig));
+  }
+}
+
+export default Database;
