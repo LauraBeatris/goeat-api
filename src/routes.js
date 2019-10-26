3ddd779eea8a7e9fd4339ee9c990a919493f0907
Laura Beatris <laurabeatriserafim@gmail.com>
2019-10-26 16:08:48 -0300

    removing test

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,7 @@ import User from './app/models/User';
 const routes = new Router();
 
 routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'laura',
-    email: 'laura@teste',
-    password_hash: '123',
-  });
-
-  return res.json(user);
+return res.json({ msg: 'hi' });
 });
 
 export default routes;
