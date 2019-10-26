966e714ea3c85a1d4d3ee3c372419073291c0a00
Laura Beatris <laurabeatriserafim@gmail.com>
2019-10-26 16:02:05 -0300

    added restaurant model

---
+++ src/app/models/Restaurant.js
@@ -0,0 +1,25 @@
+import Sequelize, { Model } from 'sequelize';
+
+import Provider from './Provider';
+
+class Restaurant extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        street_address: Sequelize.STRING,
+        number_address: Sequelize.STRING,
+        description: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+
+  static associate(models) {
+    this.myAssociation = this.hasOne(Provider);
+  }
+}
+
+export default Restaurant;
