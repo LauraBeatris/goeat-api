23f942539ab220266583e90f04e9a2396255b1bf
Laura Beatris <laurabeatriserafim@gmail.com>
2019-10-26 22:57:40 -0300

    updating user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -15,8 +15,36 @@ class UserController {
   }
 
   async update(req, res) {
-    // TO DO
-    return res.json({ ok: true });
+    const { email: passedEmail, oldPassword } = req.body;
+
+    // Finding the user register
+    const user = await User.findByPk(req.userId);
+
+    /*
+      Comparing emails
+      If it's different from the current one, we need to verify if there isn't a user
+      already using this same email.
+    */
+    if (passedEmail !== user.email) {
+      const userExists = await User.findOne({
+        where: { passedEmail },
+      });
+
+      if (userExists) {
+        return res
+          .status(404)
+          .json({ error: 'Already exists a user using this email' });
+      }
+    }
+
+    // Verifying the oldPassword
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: "Password doesn't match" });
+    }
+
+    const { id, name, email } = user.update(req.body);
+
+    return res.json({ id, name, email });
   }
 }
 
