a57dba7f7e0df67efc72cd3a121c53baab3fd91c
LauraBeatris <laurabeatriserafim@gmail.com>
2019-11-01 23:01:06 -0300

    connecting to mongo -wip

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,5 @@
 import Sequelize from 'sequelize';
-
+import mongoose from 'mongoose';
 import databaseConfig from '../config/database';
 
 import User from '../app/models/User';
@@ -13,8 +13,10 @@ const models = [User, Restaurant, Provider, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
+  // Connecting to the SQL database (MySQL)
   init() {
     this.connection = new Sequelize(databaseConfig);
 
@@ -25,6 +27,14 @@ class Database {
     );
 
     associatedModels.map(model => model.associate(this.connection.models));
+  }
+
+  // Connecting to the NoSQL database (Mongo)
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://laura:123@localhost:27017/goeat',
+      { useNewUrlParser: true, useFindAndModify: true }
+    );
   }
 }
 
