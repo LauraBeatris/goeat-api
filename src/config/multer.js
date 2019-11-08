3e22d25455899324609f3bb4b742acbe5d8ce8af
LauraBeatris <laurabeatriserafim@gmail.com>
2019-11-07 23:58:25 -0300

    added comments to s3 with multer setup

--- src/config/multer.js
+++ src/config/multer.js
@@ -15,10 +15,16 @@ const multerS3Config = multer({
   storage: multerS3({
     s3: s3Config,
     bucket: process.env.AWS_BUCKET_NAME,
+
+    // Permissions
     acl: 'public-read',
+
+    // Metadata for the file
     metadata: (req, file, cb) => {
       cb(null, { fieldName: file.fieldname });
     },
+
+    // How the name will look in the bucket
     key(req, file, cb) {
       cb(
         null,
@@ -34,26 +40,27 @@ const multerS3Config = multer({
 // Multer Middleware - Sending S3 Object URL or error
 export default (req, res) => {
   multerS3Config(req, res, error => {
-    console.log('requestOkokok', req.file);
+    console.log('File successfully sent', req.file);
     console.log('error', error);
+
     if (error) {
       console.log('errors', error);
-      res.json({ error });
+      return res.status(420).json({ error });
+    }
+    // If file not found
+    if (req.file === undefined) {
+      console.log('Error: No File Selected!');
+      res.json('Error: No File Selected');
     } else {
-      // If File not found
-      if (req.file === undefined) {
-        console.log('Error: No File Selected!');
-        res.json('Error: No File Selected');
-      } else {
-        // If Success
-        const imageName = req.file.key;
-        const imageLocation = req.file.location;
-        // Save the file name into database into profile model
-        res.json({
-          image: imageName,
-          location: imageLocation,
-        });
-      }
+      // If success
+      const imageName = req.file.key;
+      const imageLocation = req.file.location;
+
+      // TO DO -> Save the file name into database into profile model
+      return res.json({
+        image: imageName,
+        location: imageLocation,
+      });
     }
   });
 };
