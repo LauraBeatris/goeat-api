357e51c02f817bcfb714930efbac2aba2f05eb70
LauraBeatris <laurabeatriserafim@gmail.com>
2019-11-10 10:59:18 -0300

    fixing port bind

--- build/server.js
+++ build/server.js
@@ -1,6 +1,6 @@
 "use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
 
-_app2.default.listen(3333);
+_app2.default.listen(process.env.PORT, '0.0.0.0');
 
 /*
   Creating the server in two files.
