33075c917ec94fc4b4fe02dcf1d6ec27e98616ee
LauraBeatris <laurabeatriserafim@gmail.com>
2019-11-10 10:37:31 -0300

    fixed queue is not a function

--- build/queue.js
+++ build/queue.js
@@ -1,8 +1,8 @@
-"use strict";const dotenv = require('dotenv');
-const Queue = require('./lib/Queue');
+"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
+var _Queue = require('./lib/Queue'); var _Queue2 = _interopRequireDefault(_Queue);
 
-dotenv.config({
+_dotenv2.default.config({
   path: process.env.NODE_ENV === 'development' ? '.env.development' : '.env',
 });
 
-Queue.processQueue();
+_Queue2.default.processQueue();
--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,5 @@
-const dotenv = require('dotenv');
-const Queue = require('./lib/Queue');
+import dotenv from 'dotenv';
+import Queue from './lib/Queue';
 
 dotenv.config({
   path: process.env.NODE_ENV === 'development' ? '.env.development' : '.env',
