be3ccf1d887a6778053897d0368d2dcf367d4a4b
LauraBeatris <laurabeatriserafim@gmail.com>
2019-11-12 18:47:23 -0300

    fix - bucket config not receiving environment variables

--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,9 +1,7 @@
 const dotenv = require('dotenv');
 
-dotenv.config({
-  path: process.env.NODE_ENV === 'development' ? '.env.development' : '.env',
-});
+dotenv.config({});
 
-export default {
+module.exports = {
   dsn: process.env.SENTRY_URL,
 };
