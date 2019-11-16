fbb38cc8c5c9e34281805248be9342023f3c72ee
LauraBeatris <laurabeatriserafim@gmail.com>
2019-11-16 20:06:58 -0300

    changing gb to postgres - server issues

--- src/config/database.js
+++ src/config/database.js
@@ -3,11 +3,11 @@ const dotenv = require('dotenv');
 dotenv.config({});
 
 module.exports = {
-  dialect: 'mysql',
-  host: process.env.MYSQL_HOST,
-  database: process.env.MYSQL_DATABASE,
-  username: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+  dialect: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
   define: {
     timestamps: true,
     underscored: true,
